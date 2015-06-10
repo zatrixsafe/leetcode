c27fe7f191cf443aee905305325bbe9ea67c3ba7
zatrixsafe <490631401@qq.com>
2015-06-10 17:16:42 +0800

    add file

--- KthNumber.py
+++ KthNumber.py
@@ -2,20 +2,34 @@
 # File Name: KthNumber.py
 
 class Solution:
+    def ff(self,nums,k):
+        #print len(nums) + 1 -k 
+        return self.f(nums, len(nums)+1-k)
     def f(self, nums, k):
-        ans = qsort(nums, 0, len(nums)-1, k)
-
+        return self.qsort(nums, 0, len(nums)-1, k-1)
     def qsort(self, nums, beg, end, k):
-        q = sf(self, nums, beg, end) 
+        q = self.sf(nums, beg, end) 
         if k == q:
-            return nums[beg+q] 
+            return nums[q] 
         elif k > q:
-            qsort(self, nums, q+1, end, k-q)
+            return self.qsort(nums, q+1, end, k)
         else:
-            qsort(self, nums, beg, q, k)
+            return self.qsort(nums, beg, q-1, k)
 
-    def sf(self, nums, k, beg, end):
-        if beg >= end:
-            return
-        prid = beg
-        #get the number of the prid 
+    def sf(self, nums, beg, end):       
+        #print beg,end
+        if beg >= end: 
+            return end 
+        x = nums[end] 
+        cnt = beg-1
+        for i in range(beg, end):
+            if nums[i] <= x:
+                cnt += 1
+                nums[i], nums[cnt] = nums[cnt], nums[i]
+        nums[end], nums[cnt+1] = nums[cnt+1], nums[end]
+        #print nums
+        return cnt + 1 
+
+s = Solution()
+k = [5,2,4,1,3,6,0]
+print s.ff(k,2)
