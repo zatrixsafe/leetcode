6c7fc025f1f9368879492f0a1cc02dcc85f893f1
zatrixsafe <490631401@qq.com>
2015-06-11 15:37:27 +0800

    add file

--- CD3.cpp
+++ CD3.cpp
@@ -1,21 +1,40 @@
 #include<iostream>
+#include<vector>
+#include<set>
 using namespace std;
 
 class Solution{
 public:
 	bool f(vector<int>& nums, int k, int t){
-		long long tmp;
 		if(k<1 || t<0)
 			return false;
 		multiset<long long> setbox;
-		setbox.insert(nums[0]);
-		for(int i = 1; i < nums.size(); i++){
-			setbox.insert(nums[i]);
-			auto it = setbox.lower_bound(nums[i] - t);
-			if(it!=setbox.end()){
-				long long tmp = abs(*it - num[i])	
+		for(int i = 0; i < nums.size(); i++){
+			if(setbox.size() >= k + 1){
+				setbox.erase(setbox.find(nums[i-k-1]));
 			}
+			auto it = setbox.lower_bound(nums[i] - t);
+			cout<<*it<<endl;
+			if(it != setbox.end() && *it - nums[i] <= t){
+				return true;
+			}
+			setbox.insert(nums[i]);
 		}
-
+		return false;
 	}
 };
+
+int main(){
+	Solution s;
+	int nums[4] = {
+		1,10,53,50
+	};
+	vector<int> vnums(nums, nums+4);
+	bool ans = s.f(vnums, 10, 4);
+	if(ans){
+		cout<<"True"<<endl;
+	}else{
+		cout<<"False"<<endl;
+	}
+	return 0;
+}
